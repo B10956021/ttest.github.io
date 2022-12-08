9be9d74b7072e28ac5c36da30f962c7501ee51bc
B10956021 <84554062+B10956021@users.noreply.github.com>
2022-12-08 10:48:31 +0800

    測試8

---
+++ test4/tt.js
@@ -0,0 +1,7 @@
+function initViz() {
+    var containerDiv = document.getElementById("tableauViz"),
+        url = "https://public.tableau.com/views/_16704672698240/Sheet1?:language=zh-TW&:display_count=n&:origin=viz_share_link";
+
+    var viz = new tableau.Viz(containerDiv, url);
+    <hr></hr>
+}
